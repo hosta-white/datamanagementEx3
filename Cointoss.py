1e55928cbaeafbe36be36ec5d1ef9d58f3552d71
hosta <kazutoheren@gmail.com>
2024-06-29 05:46:36 +0900

    Victory message added

--- Cointoss.py
+++ Cointoss.py
@@ -20,3 +20,7 @@ for round in range(1, 4):
 
 # Print the final count of heads and tails
 print(f"Heads: {heads_count}, Tails: {tails_count}")
+if heads_count > tails_count:
+    print("You won!")
+else:
+    print("You lost!")
